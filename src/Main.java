22af50bf2ce37afabd5fa6b452a858f78f8a3e07
Amirmohammad <156039183+amirms696@users.noreply.github.com>
2026-02-28 00:18:46 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayListSubject subject = new ArrayListSubject();
+
+        // observer
+        DeleteObserver obs = new DeleteObserver();
+        subject.addObserver(obs);
+
+        // test
+        subject.append("A");
+        subject.append("B");
+        subject.append("C");
+
+        System.out.print("Before delete: ");
+        subject.printList();
+
+        subject.delete("B");
+
+        System.out.print("After delete:  ");
+        subject.printList();
+    }
+}
